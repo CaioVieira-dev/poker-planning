f251fdb4227be419318019a9d7dd81c0be5b0570
Caio Vieira <caiocesarav0208@gmail.com>
2025-06-17 23:10:43 +0000

    feat: :construction: o minimo de usabilidade

--- poker-planning-v3/src/Container.tsx
+++ poker-planning-v3/src/Container.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from "react";
+import { useCallback, useMemo, useState, type ReactNode } from "react";
 import {
   Table,
   TableBody,
@@ -13,49 +13,111 @@ export function Container() {
   return (
     <div className="flex min-h-screen w-full justify-center bg-slate-500">
       <div className="w-md bg-slate-300">
-        <div className="flex flex-wrap justify-evenly gap-2 px-4 pt-4">
-          <Card>co</Card>
-          <Card>?</Card>
-          <Card>1</Card>
-          <Card>2</Card>
-          <Card>3</Card>
-          <Card>5</Card>
-          <Card>8</Card>
-          <Card>13</Card>
-          <Card>21</Card>
-        </div>
-        <div className="">
-          <PlayersTable />
-        </div>
+        <PokerGame />
       </div>
     </div>
   );
 }
 
-function Card({ children }: { children: ReactNode }) {
-  return (
-    <div className="flex h-16 w-12 justify-center bg-slate-100 p-4 align-middle">
-      {children}
-    </div>
-  );
-}
+type playerType = {
+  name: string;
+  card: string;
+  id: number;
+};
 
 const players = [
   {
     name: "Caio",
     card: "1",
+    id: 1,
   },
   {
     name: "Testerson",
     card: "5",
+    id: 2,
   },
   {
     name: "Zé",
     card: "13",
+    id: 3,
   },
 ];
 
-function PlayersTable() {
+function PokerGame() {
+  const [game, setGame] = useState<playerType[]>(players);
+  const possibleCards = useMemo(
+    () => ["co", "?", "1", "2", "3", "5", "8", "13", "21"],
+    [],
+  );
+
+  const resetCard = useCallback((id: number) => {
+    setGame((prevGame) =>
+      prevGame.reduce<playerType[]>((acc, curr) => {
+        if (curr.id === id) {
+          acc.push({ ...curr, card: "" });
+        } else {
+          acc.push(curr);
+        }
+
+        return acc;
+      }, []),
+    );
+  }, []);
+
+  const updatePlayerCard = useCallback((id: number, card: string) => {
+    setGame((prevGame) =>
+      prevGame.reduce<playerType[]>((acc, curr) => {
+        if (curr.id === id) {
+          acc.push({ ...curr, card });
+        } else {
+          acc.push(curr);
+        }
+
+        return acc;
+      }, []),
+    );
+  }, []);
+
+  return (
+    <>
+      <div className="flex flex-wrap justify-evenly gap-2 px-4 pt-4">
+        {possibleCards.map((c, i) => (
+          <Card key={`${c}-${i}`} onClick={() => updatePlayerCard(1, c)}>
+            {c}
+          </Card>
+        ))}
+      </div>
+      <div className="">
+        <PlayersTable game={game} resetCard={resetCard} />
+      </div>
+    </>
+  );
+}
+
+function Card({
+  children,
+  onClick,
+}: {
+  children: ReactNode;
+  onClick?: () => void;
+}) {
+  return (
+    <div
+      className="flex h-16 w-12 justify-center bg-slate-100 p-4 align-middle"
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
+}
+
+function PlayersTable({
+  game,
+  resetCard,
+}: {
+  game: playerType[];
+  resetCard: (id: number) => void;
+}) {
   return (
     <Table>
       <TableCaption>
@@ -68,11 +130,11 @@ function PlayersTable() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {players.map((player) => (
-          <TableRow key={player.name}>
+        {game.map((player) => (
+          <TableRow key={player.id}>
             <TableCell className="font-medium">{player.name}</TableCell>
             <TableCell>
-              <Card> {player.card}</Card>
+              <Card onClick={() => resetCard(player.id)}>{player.card}</Card>
             </TableCell>
           </TableRow>
         ))}
